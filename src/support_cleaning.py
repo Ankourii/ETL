fa5775174c0f10b10b3a47b9bad34ea9d706ee8e
Andromachi Kountouri <akountouri@gmail.com>
2023-11-04 13:47:45 +0100

    merges

--- src/support_cleaning.py
+++ src/support_cleaning.py
@@ -151,4 +151,22 @@ def cleaning_hol_month(x):
 def cleaning_hol_day(x):
     return x.split('-')[2]
 
+def cleaning_dow(x):
+    if x==1:
+        return 'Monday'
+    elif x==2:
+        return 'Tuesday'
+    elif x==3:
+        return 'Wednesday'
+    elif x==4:
+        return 'Thursday'
+    elif x==5:
+        return 'Friday'
+    elif x==6:
+        return 'Saturday'
+    elif x==7:
+        return 'Sunday'
+    else:
+        return x
+
     
