915b2b9c0dc607e225a95b742003212e6ab4d443
Andromachi Kountouri <akountouri@gmail.com>
2023-11-04 12:22:40 +0100

    cleaningrevisado

--- src/support_cleaning.py
+++ src/support_cleaning.py
@@ -49,5 +49,106 @@ def check_nan(df: pd.DataFrame) -> None:
                )
 
     plt.show();
+
     
+
+def cleaning_month_phases(x):
+    
+    if 'Jan' in x:
+        return 1
+    elif 'Feb' in x:
+        return 2
+    elif 'Mar' in x:
+        return 3
+    elif 'Apr' in x:
+        return 4
+    elif 'May' in x:
+        return 5
+    elif 'Jun' in x:
+        return 6
+    elif 'Jul' in x:
+        return 7
+    elif 'Aug' in x:
+        return 8
+    elif 'Sep' in x:
+        return 9
+    elif 'Oct' in x:
+        return 10
+    elif 'Nov' in x:
+        return 11
+    elif 'Dec' in x:
+        return 12
+    else:
+        return x  
+
+
+    
+import re
+def cleaning_day(x):    
+    num = re.findall('\d+', x)
+    
+    
+    if num:
+        
+        return int(num[0])
+    
+    else:
+        
+        return x
+
+    
+def cleaning_phase(x):
+    if 'waning gibbous' in x.lower():
+        return 'waning gibbous'
+    elif 'last quarter' in x.lower():
+        return 'last quarter'
+    elif 'waning crescent' in x.lower():
+        return 'waning crescent'
+    elif 'new moon' in x.lower():
+        return 'new moon'
+    elif 'waxing crescent' in x.lower():
+        return 'waxing crescent'
+    elif 'first quarter' in x.lower():
+        return 'first quarter'
+    elif 'waxing gibbous' in x.lower():
+        return 'waxing gibbous'
+    elif 'full moon' in x.lower():
+        return 'full moon'
+    else:
+        return x
+    
+import re    
+def cleaning_year_ecl(x):
+    num = re.findall('\d+', x)
+    
+    if num:
+        
+        return int(num[1])
+    
+    else:
+        
+        return x
+    
+import re
+def cleaning_day_ecl(x):
+    num = re.findall('\d+', x)
+    
+    if num:
+        
+        return int(num[0])
+    
+    else:
+        
+        return x
+
+def cleaning_hol_year(x):
+    return x.split('-')[0]
+
+def cleaning_hol_month(x):
+    return x.split('-')[1]
+
+
+def cleaning_hol_day(x):
+    return x.split('-')[2]
+
     
